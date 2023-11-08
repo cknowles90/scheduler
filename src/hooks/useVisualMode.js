edde00d749350464ef98deff35df25ddc350a6e2
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-11-07 18:45:38 -0800

    useVisualMode component with transition, replace & back functions. test cases too.

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const useVisualMode = (initial) => {
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    setHistory((prev) => {
+        const updatedHistory = replace
+          ? [...prev.slice(0, prev.length - 1), newMode]
+          : [...prev, newMode]
+
+        return updatedHistory;
+    });
+  };
+
+  const back = () => {
+    setHistory((prev) => {
+      if(prev.length > 1) {
+        const updatedHistory = [...prev.slice(0, prev.length - 1)];
+        return updatedHistory;
+      }
+      return prev;
+  });
+};
+
+  return { 
+    mode: history[history.length - 1],
+    transition,
+    back,
+  };
+}
+
+export default useVisualMode;
